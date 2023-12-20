295b9bf2750f4013b847098a66a007bb13ed3315
henrique <henriqueweiand@gmail.com>
2023-12-20 20:03:05 -0300

    fixed typos

--- lib/lesson-service.ts
+++ lib/lesson-service.ts
@@ -1,15 +1,25 @@
 import { Option, Question, Lesson, LessonQuestion } from "@prisma/client";
 
+interface LessonWithQuestions extends Lesson {
+  questions: LessonQuestionWithQuestion[];
+}
+
+interface LessonQuestionWithQuestion extends LessonQuestion {
+  question: QuestionWithOptions;
+}
+
 interface QuestionWithOptions extends Question {
   options: Option[];
 }
 
-export const extractQuestions = (lesson: Lesson): QuestionWithOptions[] => {
+export const extractQuestions = (
+  lesson: LessonWithQuestions
+): QuestionWithOptions[] => {
   const questionsWithOptions: QuestionWithOptions[] = lesson.questions.map(
-    (lessonQuestion: LessonQuestion) => {
+    (lessonQuestion: LessonQuestionWithQuestion) => {
       const questionWithOptions: QuestionWithOptions = {
         ...lessonQuestion.question,
-        options: lessonQuestion.question.options,
+        options: lessonQuestion.question.options ?? [],
       };
       return questionWithOptions;
     }
