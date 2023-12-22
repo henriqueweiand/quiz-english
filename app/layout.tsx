f52f92bda0ddad6f90c8c047a3acfefe6f19ac4c
henrique <henriqueweiand@gmail.com>
2023-12-22 17:37:10 -0300

    add speed-insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
+import { Toaster } from "@/components/ui/toaster";
 import { Analytics } from "@vercel/analytics/react";
 
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         {children}
         <Toaster />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
