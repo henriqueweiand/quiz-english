4570ab473ed5b111af1481deab7c129ae6a4a982
henrique <henriqueweiand@gmail.com>
2023-12-22 20:25:35 -0300

    wip page

--- app/(website)/lesson/[id]/page.tsx
+++ app/(website)/lesson/[id]/page.tsx
@@ -1,4 +1,10 @@
 import { Button } from "@/components/ui/button";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+import { CalendarDays } from "lucide-react";
 
 interface LessonPageProps {}
 
@@ -9,7 +15,22 @@ const LessonPage = ({}: LessonPageProps) => {
         <div className="flex flex-col md:flex-row justify-between mb-4">
           <h1 className="text-4xl font-bold">Preposition of time</h1>
           <div className="flex gap-2 mt-4 md:mt-0">
-            <Button size={"sm"}>Embed</Button>
+            <HoverCard>
+              <HoverCardTrigger asChild>
+                <Button size={"sm"}>Embded</Button>
+              </HoverCardTrigger>
+              <HoverCardContent className="w-80">
+                <div className="flex justify-between space-x-4">
+                  <div className="space-y-1">
+                    <h4 className="text-sm font-semibold">Embded is easy!</h4>
+                    <p className="text-sm">
+                      1- Insert the library <br />
+                      2- Copy the code below
+                    </p>
+                  </div>
+                </div>
+              </HoverCardContent>
+            </HoverCard>
             <Button size={"sm"}>Play</Button>
           </div>
         </div>
