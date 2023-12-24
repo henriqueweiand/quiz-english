d443fe5ba5622f1deee02e35c0ada5c3712bf1cf
henrique weiand <henriqueweiand@gmail.com>
2023-12-24 16:00:16 -0300

    add link to return to the list

--- app/(website)/lesson/[id]/page.tsx
+++ app/(website)/lesson/[id]/page.tsx
@@ -5,6 +5,8 @@ import { Play } from "./_components/play";
 import { getLesson } from "@/lib/get-lesson";
 import { Embed } from "./_components/embed";
 import { extractQuestions } from "@/lib/lesson-service";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 type Tab = "embed" | "play";
 
@@ -28,7 +30,14 @@ const LessonPage = async ({ params, searchParams }: LessonPageProps) => {
     <main className="w-full min-h-screen p-4 bg-[#57c2eb] overflow-hidden">
       <div className="bg-white md:w-4/5 lg:w-3/5 mx-auto border rounded-3xl p-8 shadow-lg">
         <div className="flex flex-col md:flex-row justify-between mb-4">
-          <h1 className="text-4xl font-bold">{lesson.title}</h1>
+          <div className="flex-col justify-center items-center gap-2">
+            <Link href={'/'}>
+              <Button variant={'link'} size={'sm'} className="ml-0 pl-0">Return to the list</Button>
+            </Link>
+            <h1 className="text-4xl font-bold">
+              {lesson.title}
+            </h1>
+          </div>
           <div className="flex gap-2 mt-4 md:mt-0">
             <Options params={params} />
           </div>
