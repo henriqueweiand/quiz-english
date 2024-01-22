e876267ebe36a4536fed73a903876a146002f11d
henrique <henriqueweiand@gmail.com>
2024-01-22 20:43:54 -0300

    added rule to redirect not autorized users

--- app/(platform)/dashboard/page.tsx
+++ app/(platform)/dashboard/page.tsx
@@ -1,4 +1,6 @@
 import { Metadata } from "next";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Dashboard",
@@ -6,5 +8,15 @@ export const metadata: Metadata = {
 };
 
 export default function DashboardPage() {
+  const { userId } = auth();
+
+  if (
+    !userId ||
+    (userId !== "user_2bKX3bD3mYc4dSSLJCvOdsAQ2ag" &&
+      userId !== "user_2bKboVbNZoYdLB9ijIJ7crf8QnL")
+  ) {
+    redirect("/");
+  }
+
   return <>dash</>;
 }
