222e01861081c2d714899866d8309100cf4155ad
henrique <henriqueweiand@gmail.com>
2024-01-23 19:56:20 -0300

    added clerk provider

--- app/(platform)/dashboard/layout.tsx
+++ app/(platform)/dashboard/layout.tsx
@@ -14,16 +14,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    // <ClerkProvider>
-    <div className="flex-col md:flex">
-      <div className="border-b">
-        <div className="flex h-16 items-center px-4">
-          <MainNav className="mx-6" />
+    <ClerkProvider>
+      <div className="flex-col md:flex">
+        <div className="border-b">
+          <div className="flex h-16 items-center px-4">
+            <MainNav className="mx-6" />
+          </div>
         </div>
-      </div>
 
-      <div className="flex-1 space-y-4 p-8 pt-6">{children}</div>
-    </div>
-    // </ClerkProvider>
+        <div className="flex-1 space-y-4 p-8 pt-6">{children}</div>
+      </div>
+    </ClerkProvider>
   );
 }
