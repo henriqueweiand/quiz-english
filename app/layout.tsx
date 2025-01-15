acd73afff8a968c8a33f483ac84908102974c886
henrique weiand <henriqueweiand@gmail.com>
2025-01-15 19:34:24 -0300

    removed vercel libraries

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,4 @@
 import { Toaster } from "@/components/ui/toaster";
-import { Analytics } from "@vercel/analytics/react";
-
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider";
